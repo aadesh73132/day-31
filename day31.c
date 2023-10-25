8c9c1b72fc1ab835cc2c8e7ce2f29e99c66eb056
aadesh73132 <145355002+aadesh73132@users.noreply.github.com>
2023-10-25 19:50:25 +0530

    Add files via upload

---
+++ day31.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    int num;
+
+    printf("enter the number you want multipication table\n");
+    scanf("%d",&num);
+    printf("the multiplication table of number is %d\n",num);
+
+   
+    printf("%d*1=%d\n",num,num*1);
+    printf("%d*2=%d\n",num,num*2);
+    printf("%d*3=%d\n",num,num*3);
+    printf("%d*4=%d\n",num,num*4);
+    printf("%d*5=%d\n",num,num*5);
+    printf("%d*6=%d\n",num,num*6);
+    printf("%d*7=%d\n",num,num*7);
+    printf("%d*8=%d\n",num,num*8);
+    printf("%d*9=%d\n",num,num*9);
+    printf("%d*10=%d\n",num,num*10);
+
+
+
+
+    return 0;
+}
